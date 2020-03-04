e57519bfee65bc5a04b56ef58d621345a5d3966c
Anda Achimescu <anda.a@live.ca>
2020-03-04 13:08:04 -0500

    Starting

---
+++ p3/p3/p3/QuadTree.h
@@ -0,0 +1,20 @@
+//
+//  QuadTree.h
+//  p3
+//
+//  Created by Anda Achimescu on 2020-03-04.
+//  Copyright © 2020 Anda Achimescu. All rights reserved.
+//
+
+#ifndef QuadTree_h
+#define QuadTree_h
+
+class QuadTree {
+public:
+    
+    
+private:
+    
+};
+
+#endif /* QuadTree_h */
---
+++ p3/p3/p3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  p3
+//
+//  Created by Anda Achimescu on 2020-03-04.
+//  Copyright © 2020 Anda Achimescu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
