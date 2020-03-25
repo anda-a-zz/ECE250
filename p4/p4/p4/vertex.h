114e7c2dc0650ae7709b9925b5e6893592d646b7
Anda Achimescu <anda.a@live.ca>
2020-03-25 12:29:52 -0400

    Add vertex

---
+++ p4/p4/p4/vertex.h
@@ -0,0 +1,17 @@
+//
+//  vertex.h
+//  p4
+//
+//  Created by Anda Achimescu on 2020-03-25.
+//  Copyright © 2020 Anda Achimescu. All rights reserved.
+//
+
+#ifndef vertex_h
+#define vertex_h
+
+class vertex {
+public:
+private:
+}
+
+#endif /* vertex_h */
