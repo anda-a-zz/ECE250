abb3c6cc529806030706681044edda3004f7b744
Anda Achimescu <anda.a@live.ca>
2020-03-31 15:28:41 -0400

    Fixed in order to keep track of multiple linked lists

--- p4/p4/p4/disjointset.h
+++ p4/p4/p4/disjointset.h
@@ -9,24 +9,33 @@
 #ifndef disjointset_h
 #define disjointset_h
 
-#include "node.H"
+#include "node.h"
+#include "linkedlist.h"
 
 class DisjointSet {
 public:
     DisjointSet();                      // Constructor sets pointers to nullptr
     ~DisjointSet();                     // Destructor sets pointers to nullptr and deletes
-    string make_set(Vertex x);
-    string find_set(Vertex x);
+    Node * make_set(Vertex x);
+    Node * find_set(Vertex x);
     void union_lists(Vertex x, Vertex y);
     void clear();
-    
+    void size(int n);
     
 private:
-    Node *set_head;
-    Node *set_tail;
-    string setID;
-    void remove_node();
-    
+    std::vector<LinkedList> disjoint_lists;
+    size_t max_size;
+    size_t set_size;
 };
 
 #endif /* disjointset_h */
+
+// In my disjoint_lists vector:
+// ie: n = 5
+// | 0 | 1 | 2 | 3 | 4 |
+// union 0 & 4
+// | 0 | 1 | 2 | 3 | 0 |
+// | 4 |
+// find set (vertex = 4)
+// if vertex = vector[vertex] then found!
+// else, vector[vector[vertex]] (go to the next value inside)
--- p4/p4/p4/disjointset_functions.cpp
+++ p4/p4/p4/disjointset_functions.cpp
@@ -10,8 +10,8 @@
 #include "disjointset.h"
 
 DisjointSet::DisjointSet() {
-    set_head = nullptr;
-    set_tail = nullptr;
+    
+    set_size = 0;
 }
 
 DisjointSet::~DisjointSet() {
@@ -20,43 +20,42 @@ DisjointSet::~DisjointSet() {
 
 // Clears the content of the LinkedList and will output success
 void DisjointSet::clear(){
-    Node *current_node = set_head;
-    
-    while (current_node != nullptr) {
-        remove_node();
-        current_node = current_node -> next_node;
+    for (int i = 0; i < max_size; i++) {
+        disjoint_lists[i].clear();
     }
+    disjoint_lists.clear();
+    disjoint_lists.shrink_to_fit();
     
-    current_node = nullptr;
-    set_head = nullptr;
-    set_tail = nullptr;
+    max_size = 0;
+    set_size = 0;
 }
 
-// Remove element from the front or back and will output success/failure
-void DisjointSet::remove_node(){
-    // check if LinkedList is empty
-    if (set_head == nullptr && set_tail == nullptr) {
-        return;
+void DisjointSet::size(int n) {
+    disjoint_lists.resize(n);
+    max_size = n;
+    Vertex extra(-1);
+    for(int i = 0; i < n; ++i){
+        disjoint_lists[i] = LinkedList();
     }
-    
-    // delete the last node in the list
-    Node *current_node = set_tail;
-    set_tail = current_node -> prev_node;
-    if (set_tail != set_head)
-        set_tail -> next_node = nullptr;
-    else
-        set_head = nullptr;
-    // remember to delete the node
-    delete current_node;
-}
-
-string DisjointSet::make_set(Vertex x) {
-    
 }
 
 
-string DisjointSet::find_set(Vertex x) {
-    
+Node * DisjointSet::make_set(Vertex x) {
+    disjoint_lists[x.key()].insert_node(x);
+    set_size = set_size + 1;
+    return disjoint_lists[x.key()].list_head;
+}
+
+
+// if vertex = vector[vertex] then found!
+// else, vector[vector[vertex]] (go to the next value inside)
+// should be constant time
+Node * DisjointSet::find_set(Vertex x) {
+    if (disjoint_lists[x.key()].get_parent_key() == x.key()) {
+        return disjoint_lists[x.key()].list_head;
+    } else {
+        return disjoint_lists[disjoint_lists[x.key()].get_parent_key()].list_head;
+    }
 }
 
 void union_lists(Vertex x, Vertex y) {
